f495cc0a380c29da9b6a5f8f209dc00354c19521
Patricia Eliseu <patyeliseu4@gmail.com>
2024-07-29 16:24:35 -0300

    final

---
+++ src/main/java/com/example/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.domain.repository;
+
+import com.example.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
---
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.service.impl;
+
+import com.example.domain.model.User;
+import com.example.domain.repository.UserRepository;
+import com.example.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
